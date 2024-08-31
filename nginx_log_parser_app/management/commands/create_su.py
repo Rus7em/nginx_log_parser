25b9fd5350fef129f1003484b12bbe57b496747a
tippy <tippy@mail.com>
2024-08-31 21:17:10 +0300

    add execution from compose

---
+++ nginx_log_parser_app/management/commands/create_su.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+import os
+
+
+class Command(BaseCommand):
+    help = "Superuser creating"
+
+    def handle(self, *args, **options):
+        username = os.getenv('DJANGO_SUPERUSER_USERNAME')
+        password = os.getenv('DJANGO_SUPERUSER_PASSWORD')
+        email = os.getenv('DJANGO_SUPERUSER_EMAIL')
+        
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username,
+                                          password=password,
+                                          email=email)
+            self.stdout.write(self.style.SUCCESS(f'Superuser "{username} created'))
+        else:
+            self.stdout.write(self.style.WARNING(f'Superuser "{username} already exists'))
